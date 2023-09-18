87cdb0c6b0f5a46f0050a1b7ca1544ffd5063546
Kserhi <movie.to.48@gmail.com>
2023-09-19 00:21:07 +0300

    ghbdn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Routes } from 'react-router-dom'; // Додайте Routes та Route
 
+import NavBar from './NavBar';
 import Registration from './components/Registration';
 import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,21 +12,27 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/registration" className="nav-link">Реєстрація</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">Вхід</Link>
-            </li>
-          </ul>
-        </nav>
+  
+        <NavBar /> {/* Викликаємо навігаційну панель */}
+        <div><p className="text-center">Вітаю шановний користувач</p>
+      <p>Це головна сторінка сайту пройдіть реєстрацію або залогінтесь</p>
+    </div>
 
-        <Routes>
-          <Route path="/registration" element={<Registration />} />
+
+
+      <Routes>
+      <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
-        </Routes>
+      </Routes>
+      
+      <Switch>
+         
+          <Route path="/registration" component={Registration} />
+          <Route path="/login" component={Login} />
+          
+        </Switch>
+
+        
       </div>
     </Router>
   );
